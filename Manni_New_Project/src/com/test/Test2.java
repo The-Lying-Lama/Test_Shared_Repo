4dc4bce82f8d2316591d6d75c8b127291a02183d
The-Lying-Lama <maddypeas@gmail.com>
2016-06-16 08:14:27 -0400

    test commit

---
+++ Manni_New_Project/src/com/test/Test2.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello 1");
+		System.out.println("Hello 2");
+		System.out.println("Hello 3");
+
+	}
+
+}
